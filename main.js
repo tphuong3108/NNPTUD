1d5bef1acc4c7027fc393e67c9761dd3725929d1
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-09-18 10:19:52 +0700

    Filter

--- main.js
+++ main.js
@@ -12,13 +12,54 @@ let posts = [
       id: "2",
       title: "another title",
       views: 200
-    }
+    },{
+        id: "3",
+        title: "a title 3",
+        views: 300
+      },
+      {
+        id: "4",
+        title: "another title 4",
+        views: 100
+      }
   ]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/posts', (req, res) => {
+    let queries = req.query;
+    let views = queries.views;
+    let views_lte = queries.views_lte;
+    let views_gte = queries.views_gte;
+    let title = queries.title;
+    let title_like = queries.title_like;
+    
+    if(views){
+        posts = posts.filter(
+            p=>p.views==views
+        )
+    }
+    if(views_lte){
+        posts = posts.filter(
+            p=>p.views<=views_lte
+        )
+    }
+    if(views_gte){
+        posts = posts.filter(
+            p=>p.views>=views_gte
+        )
+    }
+    if(title_like){
+        posts  = posts.filter(
+            p=>p.title.includes(title_like)
+        )
+    }
+    if(title){
+        posts  = posts.filter(
+            p=>p.title == title
+        )
+    }
     res.send(posts)
 })
 app.get('/posts/:id', (req, res) => {
