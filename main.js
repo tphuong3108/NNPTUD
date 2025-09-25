4894f88a2492cde7eeb93db297de9434d3cf65d8
TUNGNT <nguyenthanhtunnghutechsg@gmail.com>
2025-09-25 08:20:45 +0700

    CUD

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+const { json } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
 let fs = require('fs')
+app.use(json())
 
 
 app.get('/', (req, res) => {
@@ -9,7 +11,7 @@ app.get('/', (req, res) => {
 })
 app.get('/posts', (req, res) => {
     let posts = fs.readFileSync('./db.json');
-    posts = JSON.parse(posts).posts;
+    posts = JSON.parse(posts).posts.filter(p=>!p.isDeleted);
     
     let queries = req.query;
     let views = queries.views;
@@ -46,6 +48,8 @@ app.get('/posts', (req, res) => {
     res.send(posts)
 })
 app.get('/posts/:id', (req, res) => {
+    let posts = fs.readFileSync('./db.json');
+    posts = JSON.parse(posts).posts.filter(p=>p.id==true);
     let id = req.params.id;
     let post = posts.filter(
         p=>p.id==id
@@ -61,7 +65,72 @@ app.get('/posts/:id', (req, res) => {
         });
     }
 })
-
+app.post('/posts',(req,res)=>{
+    let newPost = {
+        id:req.body.id,
+        title:req.body.title,
+        views:req.body.views
+    }
+    let dataFile = fs.readFileSync('./db.json');
+    posts = JSON.parse(dataFile).posts;
+    
+    posts.push(newPost);
+    let dataWrite = {
+        posts:posts
+    }
+    fs.writeFileSync('./db.json',JSON.stringify(dataWrite));
+    res.send({
+        success:true,
+        data:newPost
+    });
+});
+app.delete('/posts/:id',(req,res)=>{
+    let dataFile = fs.readFileSync('./db.json');
+    posts = JSON.parse(dataFile).posts;
+    let id = req.params.id;
+    let getPosts = posts.filter(p=>p.id==id);
+    if(getPosts.length>0){
+        let getPost = getPosts[0];
+        getPost.isDeleted=true;
+        let dataWrite = {
+            posts:posts
+        }
+        fs.writeFileSync('./db.json',JSON.stringify(dataWrite));
+        res.send({
+            success:true,
+            data:"Xoa thanh cong"
+        })
+    }else{
+        res.status(404).send({
+            success:true,
+            data:"ID not found"
+        });
+    }
+})
+app.put('/posts/:id',(req,res)=>{
+    let dataFile = fs.readFileSync('./db.json');
+    posts = JSON.parse(dataFile).posts;
+    let id = req.params.id;
+    let getPosts = posts.filter(p=>p.id==id);
+    if(getPosts.length>0){
+        let getPost = getPosts[0];
+        getPost.title = req.body.title?req.body.title:getPost.title;
+        getPost.views = req.body.views?req.body.views: getPost.views;
+        let dataWrite = {
+            posts:posts
+        }
+        fs.writeFileSync('./db.json',JSON.stringify(dataWrite));
+        res.send({
+            success:true,
+            data:getPost
+        })
+    }else{
+        res.status(404).send({
+            success:true,
+            data:"ID not found"
+        });
+    }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
